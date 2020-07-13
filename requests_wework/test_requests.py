4184bab7c21dfb42f01bf96f7d4b91b81ac817f3
lenovo <306262361@qq.com>
2020-07-13 11:47:14 +0800

    更新

--- requests_wework/test_requests.py
+++ requests_wework/test_requests.py
@@ -1,45 +1,71 @@
+
+import re
+from requests_wework.auto_data import auto_name
+import pytest
 import requests
 
 _corpid = 'ww7c598bbdd3aa6f03'
 _corpsecret = 'kPD0yEMpM6PhxMYY2h1HO-GXoLmxIa2FMvYdWSpmUqw'
-
+_name=auto_name()
 
 def test_demo():
     r = requests.get('http://api.gethub.com/enents')
     print(r.json)
 
-
+@pytest.fixture(scope='session')
 def test_token():
     res = requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={_corpid}&corpsecret={_corpsecret}')
-    print(res.json()['access_token'])
     return res.json()['access_token']
 
 
-def test_get():
-    res = requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={test_token()}&userid=zhangsan')
-    print(res.json())
+def test_get(userid,test_token):
+    res = requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={test_token}&userid={userid}')
+    return res.json()
 
 
-def test_create():
+def test_create(userid,name,mobile,test_token):
     data = {
-        "userid": "zhangsan",
-        "name": "张三",
-        "mobile": "13800000000",
+        "userid": userid,
+        "name": name,
+        "mobile": mobile,
         "department": [1]
     }
-    res = requests.post(f'https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={test_token()}', json=data)
-    print(res.json())
+    res = requests.post(f'https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={test_token}', json=data)
+    return res.json()
 
 
-def test_update():
+def test_update(userid,name,mobile,test_token):
     data={
-        "userid": "zhangsan",
-        "name": "李四",
-        "mobile": "13800000999"
+        "userid": userid,
+        "name": name,
+        "mobile": mobile
     }
-    res=requests.post(f'https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={test_token()}',json=data)
-    print(res.json())
+    res=requests.post(f'https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={test_token}',json=data)
+    return res.json()
 
-def test_delete():
-    res=requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={test_token()}&userid=zhangsan')
-    print(res.json())
+def test_delete(userid,test_token):
+    res=requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={test_token}&userid={userid}')
+    return res.json()
+
+def test_create_data():
+
+    data=[("userid"+str(x),
+               'zhangsan'+str(x),
+               "138%08d"%x) for x in range(10)]
+    return data
+
+@pytest.mark.parametrize('userid,name,mobile',test_create_data())
+def test_all(userid,name,mobile,test_token):
+    try:
+        assert 'created'==test_create(userid,name,mobile,test_token)['errmsg']
+    except AssertionError as a:
+        if 'mobile existed' in a.__str__():
+            re_userid=re.findall("mobile existed:(.*)",a.__str__())[-1]
+            assert 'deleted'==test_delete(re_userid,test_token)['errmsg']
+            assert 60111 == test_get(re_userid,test_token)['errcode']
+            assert 'created' == test_create(userid, name, mobile,test_token)['errmsg']
+    assert name==test_get(userid,test_token)['name']
+    assert 'updated'==test_update(userid,name+name,mobile,test_token)['errmsg']
+    assert name+name == test_get(userid,test_token)['name']
+    assert 'deleted'==test_delete(userid,test_token)['errmsg']
+    assert 60111== test_get(userid,test_token)['errcode']
